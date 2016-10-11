0153cf9e2662173cf79aae30f46fde3573d4b851
Sam Vickars <vicks201@newschool.edu>
2016-10-11 03:22:07 +0000

    work on interactivity

--- qualData/sketch.js
+++ qualData/sketch.js
@@ -3,50 +3,37 @@ var cF = 'rgba(66,173,73,' + cAlpha + ')';
 var cNF = 'rgba(255,0,0,' + cAlpha + ')';
 var cPF = 'rgba(19,116,147,' + cAlpha + ')';
 
+var cnv;
 var latLngM;
 var cSizeM;
 var cSizeD;
 var cSizeA;
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+    cnv = createCanvas(windowWidth, windowHeight);
     background(255);
     textAlign(CENTER, CENTER);
     loadTable('dataSources/freedom2016.tsv', 'tsv', 'header', showData2016);
     rectMode(CENTER);
-    
-
-    // y2016 = createDiv('2016');
-    // y2016.position(200,100);
-    // y2016.style("font-family","Noto Sans");
-    // y2016.style("cursor","default");
-    // y2016.mousePressed(showData2016);
-    // y2015 = createDiv('2015');
-    // y2015.position(100,100);
-    // y2015.style("font-family","Noto Sans");
-    // y2015.style("cursor","default");
-    // y2015.mousePressed(showData2015);
     
     latLngM = 2;
     cSizeM = 4;
     cSizeD = 500000;
     cSizeA = 5;
 
-    zoomIn = createButton("+");
-    zoomIn.position(100,19);
-    zoomIn.style("cursor","pointer");
-    zoomIn.mousePressed(zoomInskies);
-    
-    zoomOut = createButton("-");
-    zoomOut.position(75,19);
-    zoomOut.style("cursor","pointer");
-    zoomOut.mousePressed(zoomOutskies);
+    // zoomIn = createButton("+");
+    // zoomIn.position(100,19);
+    // zoomIn.style("cursor","pointer");
+    // zoomIn.mousePressed(zoomInskies);
     
     // zoomOut = createButton("-");
-    // zoomOut.position(36,19);
-    // zoomOut.mousePressed(zO);
+    // zoomOut.position(75,19);
+    // zoomOut.style("cursor","pointer");
+    // zoomOut.mousePressed(zoomOutskies);
+    
     
 }
+
 
 function zoomInskies() {
         latLngM = latLngM * 2;
@@ -103,8 +90,7 @@ function showData2016(data) {
 
         fill(cColour);
         textSize(((4 * cSize) / 500000) + 5);
-        // textFont('Roboto Condensed');
-
+        
         // textSize(((4*cSize)/500000)+5);
         // txt = createDiv(text(cStatus.toUpperCase(),(cLng*2)+(width/2),(cLat*-2)+(height/2)));
         sta = createDiv(cStatus);
@@ -115,8 +101,11 @@ function showData2016(data) {
         sta.style("text-transform", "Uppercase");
         sta.style("cursor","default");
 
-        // textSize(24);
-        // text(cName, 100, 100);
+        // var cStatusBox = font.textBounds(cStatus, (cLng * latLngM) + (width / 2) - textWidth(cStatus)/2, (cLat * -latLngM) + (height / 2) - (cSizeM*cSize/cSizeD)+cSizeA);
+        // fill(255);
+        // stroke(0);
+        // rect(cStatusBox.x, cStatusBox.y, cStatusBox.w, cStatusBox.h);
+        
         
         cou = createDiv(cName);
         cou.position(mouseX,mouseY);
@@ -129,18 +118,14 @@ function showData2016(data) {
 
         sta.mouseOver(hoverIn);
         sta.mouseOut(hoverOut);
-
-        function hoverIn() {
-            // fill(255);
-            // rect(mouseX, mouseY, 100, 100);
-            cou.position(mouseX,mouseY);
-            cou.style("visibility","visible");
-        }
-
-        function hoverOut() {
-            background(255);
-            cou.style("visibility","hidden");
-        }
-
     }
+}
+
+function hoverIn() {
+    cou.position(mouseX,mouseY);
+    cou.style("visibility","visible");
+}
+
+function hoverOut() {
+    cou.style("visibility","hidden");
 }
