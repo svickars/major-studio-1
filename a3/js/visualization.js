bcc9c388ac1ba64b2e27fb2617d26bfab38d02b0
Sam Vickars <vicks201@newschool.edu>
2016-11-15 03:49:14 +0000

    add dynamic width headline, add 'stories' to each country, add contextual information 'buubles', add comparisons, add comparison swticher, add democracy index to each country's pop-up

--- a3/js/visualization.js
+++ a3/js/visualization.js
@@ -64,7 +64,6 @@ var x = d3.scaleTime().range([0, width]);
 var y = d3.scaleLinear().range([height, 0]);
 var y2 = d3.scaleLinear().range([height+250, height+50]);
 
-
 // Add axes
 var x_axis = svg.append("g")
 	.attr("transform", "translate(0," + (height) + ")")
@@ -84,7 +83,7 @@ yLabel1.html("Estimated Score <span class='info-icon' id='icon1'><a><i class='fa
 yLabel1.style("left", (visualizationWidth/2)-(1048/2)-40+"px");
 
 var infopop1 = d3.select("#visualization").append("div").attr("class", "infopop");
-infopop1.html("<h2>Estimated Governance Indicator Scores</h2><h6>Worldwide Governance Indicator scores are provided by the World Bank for six dimensions of governance. The data used above are estimated scores on the aggregate indicator, in units of a standard normal distribution, ranging from approximately -3 to +3.</br></br> The WGI are composite indicators based on over 30 underlying <a href='http://info.worldbank.org/governance/wgi/index.aspx#doc-sources'>data sources</a>. These data sources are rescaled and combined to create the six aggregate indicators using a statistical methodology known as an unobserved components model. </h6>");
+infopop1.html("<h2>Estimated Governance Indicator Scores</h2><h6>Worldwide Governance Indicator scores are provided by the World Bank for six dimensions of governance. The data used are estimated scores on the aggregate indicator, in units of a standard normal distribution, ranging from approximately -3 to +3.</br></br> The WGI are composite indicators based on over 30 underlying <a href='http://info.worldbank.org/governance/wgi/index.aspx#doc-sources'>data sources</a>. These data sources are rescaled and combined to create the six aggregate indicators using a statistical methodology known as an unobserved components model. </h6>");
 infopop1.style("left", (visualizationWidth/2)-(1048/2)+40+"px");
 infopop1.style("top", "216px");
 
@@ -164,7 +163,7 @@ var line = d3.line()
 		return x(new Date(d.year, 0, 1));
 	})
 	.y(function(d) {
-		return y(d.value);
+		return y(parseFloat(d.value));
 	});
 
 var line3 = d3.line()
@@ -211,9 +210,9 @@ function drawVisual(refreshLine) {
 		return (d.region == region) && d.indicator == indicator;
 	});
 	var iSelectWidth = getTextWidth(indicator, "22px arial");
-	d3.select("#indicator").style("width", iSelectWidth+16+"px");
+	d3.select("#indicator").style("width", iSelectWidth+20+"px");
 	var rSelectWidth = getTextWidth(region, "22px arial");
-	d3.select("#region").style("width", rSelectWidth+16+"px");
+	d3.select("#region").style("width", rSelectWidth+20+"px");
 	//Update title
 	d3.select(".graph-title").html(indicator + " for " + region + " Region")
 		// Nest the entries by country
@@ -296,7 +295,7 @@ function drawVisual(refreshLine) {
 				var chart2 = d3.select(".lbIn")
 					.append("svg")
 					.attr("width", 1200)
-					.attr("height", 500)
+					.attr("height", 1000)
 					.append("g")
 					.attr("transform", "translate(85,40)");
 
@@ -321,6 +320,13 @@ function drawVisual(refreshLine) {
 					.attr("y", 19)
 					.text("Governance Indicator Scores for " + country + " 2006-2014")
 					.style("opacity", ".6");
+				chart2.append("text")
+					.attr("class", "lbtitle")
+					.attr("text-anchor", "middle")
+					.attr("x", 671)
+					.attr("y", 469)
+					.text("Democracy Index, 2006-2014")
+					.style("opacity", ".6");
 				
 				chart2.append("text")
 					.attr("id", "lbSubtitle")
@@ -332,18 +338,54 @@ function drawVisual(refreshLine) {
 					.style("font-size", "10px")
 					.style("font-weight", "400")
 					.style("font-style", "italic");
-
+				
+				var x4 = d3.scaleTime().range([460,888]);
+				var y4 = d3.scaleLinear().range([650, 455]);
+				
 				// Add the X Axis
 				var x_axis3 = chart2.append("g")
 					.attr("transform", "translate(0," + height + ")")
 					.style("stroke-linecap", "round")
 					.style("stroke-dasharray", ".1,20");
+				
+				var x_axis4 = chart2.append("g")
+					.attr("transform", "translate(0, 650)");
 
 				// Add the Y Axis
 				var y_axis3 = chart2.append("g")
 					.style("stroke-linecap", "round")
 					.style("stroke-dasharray", ".1,20");
+				
+				var y_axis4 = chart2.append("g")
+					.attr("transform", "translate(460, 0)")
+					.style("stroke-linecap", "round")
+					.style("stroke-dasharray", ".1,20");
 
+				var lbLine = d3.line()
+					.curve(d3.curveMonotoneX)
+					.x(function(d) {
+						return x4(new Date(d.year, 0, 1));
+					})
+					.y(function(d) {
+						return y4(d.value);
+					});
+				var lbLineSSA = d3.line()
+					.curve(d3.curveMonotoneX)
+					.x(function(d) {
+						return x4(new Date(d.year, 0, 1));
+					})
+					.y(function(d) {
+						return y4(d.ssa);
+					});
+				var lbLineW = d3.line()
+					.curve(d3.curveMonotoneX)
+					.x(function(d) {
+						return x4(new Date(d.year, 0, 1));
+					})
+					.y(function(d) {
+						return y4(d.world);
+					});
+				
 				// Get the data
 				d3.csv("governanceIndicators-r.csv", function(error, data) {
 					data = data.filter(function(row) {
@@ -362,12 +404,120 @@ function drawVisual(refreshLine) {
 						return new Date(d.year, 0, 1);
 					}));
 					y.domain([-3, 3]);
-
+					
+					x4.domain(d3.extent(data_subset, function(d) {
+						return new Date(d.year, 0, 1);
+					}));
+					y4.domain([0, 10]);
+					
 					var strokeHex = color(index);
 					var strokeRGB = hexToRgbA(strokeHex);
 					
+					// var sData = data.filter(function(row) {
+					// 	return row['country'] == country;
+					// }).entries(sData);
+					
+					// sData.forEach(function(d) {
+					// 	d.measure = d.measure;
+					// });
+
+					// sData = d3.nest().key(function(d) {
+					// 	return d.measure;
+					// }).entries(sData);
+					
+					var sCountry = data[6].values[6].country;
+					var sYear = data[6].values[6].year;
+					var sOverview = data[6].values[6].overview;
+					var sdStatus = data[6].values[6].democraticStatus;
+					var sfStatus = data[6].values[6].freedomStatus;
+					var sCL = data[6].values[6].cL;
+					var sPL = data[6].values[6].pL;
+					var sF = data[6].values[6].freedomRating;
+					var sURL = data[6].values[6].url;
+					
+					var storytime = lbIn.append("div").attr("class", "storytime");
+					storytime.html("<div class='s1'><div class='sCountry'><span id='sCountry'>"+sCountry+"</span><div id='sYear'>"+sYear+"<span class='info-icon' id='icon5'><a><i class='fa fa-info-circle' aria-hidden='true'></i></a></span></div></div><div class='sStats'>Democratic? <strong>"+sdStatus+"</strong>&nbsp&nbsp Free? <strong>"+sfStatus+" </strong>&nbsp&nbspFreedom Rating: <strong>"+sF+" </strong>&nbsp&nbspCivil Liberties: <strong>"+sCL+"</strong>&nbsp&nbsp Politcal Rights: <strong>"+sPL+"</strong>&nbsp&nbsp</div><div class='sOverview'>"+sOverview+"</div><div class='sRead'><a href="+sURL+">Read More at Freedom House</a></div></div><div class='s2'><div class='sCharts'></div></div>");
+					d3.select(".sCountry").style("color", color(index));
+					d3.select(".sStats").style("color", color(index));
+					d3.select(".sRead").style("color", color(index));
+					d3.select(".sRead").style("border-color", color(index));
+					d3.select("#sCountry").style("border-color", color(index));
+					
+					var infopop5 = d3.select("#icon5").append("div")
+						.attr("class", "infopop-top")
+						.attr("id", "infopop5")
+						.style("width", "300px")
+						.style("top", "35px");
+						infopop5.html("<h6><p>This data is based on Freedom House's Freedom in World Report, a yearly report that measures the degrees of civil liberties and politcal rights in every nation in the world. The data shown here are based on the 2015 report which gives scores for 2014.</p><p>The democratic question is based on the Economist's Democratic Index, as is the chart to the right, which maps "+sCountry+"'s democratic score across 2006-2014. The world average as well as the Sub-Saharan Africa average are shown in black.</p><p><strong>Civil Liberties and Political Rights:</strong> Each country is assigned a score on a series of 25 indicators, for an aggregate score of up to 100. These scores are used to determine two numerical ratings with a rating of 1 representing the most free conditions and 7 the least free.</p></h6><div class='footnote'>More details at <a href='https://freedomhouse.org/sites/default/files/FH_FITW_Report_2016.pdf'>freedomhouse.org</a></div>");
+						
+					d3.select("#icon5").on("mouseover", function(d) {
+						infopop5.style("display", "inline-block");
+					});
+					d3.select("#icon5").on("mouseout", function(d) {
+						infopop5.style("display", "none");
+					});
+					d3.select(".sOverview").on("mouseover", function(d) {
+						d3.select(".sOverview").style("height", "auto");
+						d3.select(".sOverview").style("min-height", "124px");
+					})
+					d3.select(".sOverview").on("mouseout", function(d) {
+						d3.select(".sOverview").style("height", "124px");
+					})
+					
+					var lbLabels = chart2.append("text")
+						.attr("x", "895")
+						.attr("y", y4(parseFloat(data[6].values[6].value))+2)
+						.text("Democracy Score: "+data[6].values[6].value)
+						.style("fill", color(index))
+						.style("font-size", 8)
+						.style("text-transform", "uppercase");
+					if (data[6].values[6].value == "0") {
+						lbLabels.text("unavailable");
+					}
+					var lbLabelsWA = chart2.append("text")
+						.attr("x", "895")
+						.attr("y", y4(parseFloat(data[6].values[6].world))+2)
+						.text("World Average: " + data[6].values[6].world)
+						.style("fill", "black")
+						.style("font-size", 8)
+						.style("text-transform", "uppercase");
+					var lbLabelsSSA = chart2.append("text")
+						.attr("x", "895")
+						.attr("y", y4(parseFloat(data[6].values[6].ssa))+2)
+						.text("SSA Average: " + data[6].values[6].ssa)
+						.style("fill", "grey")
+						.style("font-size", 8)
+						.style("text-transform", "uppercase");
+					var lbPath = chart2.append("path")
+						.datum(data[6].values)
+						.attr("class", "lbLine")
+						.style("stroke", color(index))
+						.style("fill", "none")
+						.attr("d", lbLine);
+					var lbPathssa = chart2.append("path")
+						.datum(data[6].values)
+						.attr("class", "lbLine")
+						.style("stroke", "grey")
+						.style("fill", "none")
+						.attr("d", lbLineSSA);
+					var lbPathW = chart2.append("path")
+						.datum(data[6].values)
+						.attr("class", "lbLine")
+						.style("stroke", "black")
+						.style("fill", "none")
+						.attr("d", lbLineW);
+					
+					if (data[6].values[0] == 0){
+						lbPath.style("display", "none");
+					}
+					
+					x_axis4.attr("class", "x_axis axis").call(d3.axisBottom(x4)
+							.tickSizeInner(-195));
+					y_axis4.attr("class", "y_axis axis").call(d3.axisLeft(y4)
+							.tickSizeInner(-428));
+					
 					// Add the valueline path.
-					for (var i = 0; i < data.length; i++) {
+					for (var i = 0; i < 6; i++) {
 						var chart2Labels = chart2.append("text")
 							.attr("x", 895)
 							.attr("y", y(parseFloat(data[i].values[8].value)))
@@ -457,7 +607,7 @@ function drawVisual(refreshLine) {
 									return x(new Date(data[i].values[h].year, 0, 1));
 								})
 								.attr("cy", function(d) {
-									return y(data[i].values[h].value);
+									return y(parseFloat((data[i].values[h].value)));
 								})
 								.attr("r", 3)
 								.style("z-index", "999")
